c63ce4ca142e9d9f9f40b6d7e493abf054d645ac
Martin Canton <martin.canton96@hotmail.com>
2024-05-28 19:17:53 -0300

    agregado de data, y libreria router

--- src/components/ItemsListContainer/ItemsListContainer.jsx
+++ src/components/ItemsListContainer/ItemsListContainer.jsx
@@ -3,7 +3,7 @@ import './ItemsListContainer.css';
 export const ItemsListContainer = ({greeting}) => {
   return (
     <div>
-        <h1 className="titlewelcome">{greeting}</h1>        
+       <h1 className="titlewelcome">{greeting}</h1>   
     </div>
   )
 }
